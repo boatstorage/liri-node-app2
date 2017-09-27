869c612c1e399320e8456fb5240c420f6b248315
marina lyulko <marinalyulko@gmail.com>
2017-09-27 14:54:29 -0700

    twitter kinda works

--- liri.js
+++ liri.js
@@ -28,6 +28,21 @@ switch(action) {
 
 
 function myTweets() {
+	var Twitter = require('twitter');
+
+	var client = new Twitter({
+  	consumer_key: '',
+  	consumer_secret: '',
+  	access_token_key: '',
+  	access_token_secret: ''
+	});
+
+	client.get('statuses/user_timeline', function(error, tweets, response) {
+  		if(error) throw error;
+  		console.log(tweets);  // The favorites. 
+  		console.log(response);  // Raw response object. 
+		
+		});
 
 }
 
